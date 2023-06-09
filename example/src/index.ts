255845dafbf0d0c26e39c0e4350f1db7c3de1f67
naporin0624 <naporin0624@gmail.com>
2023-06-09 19:49:49 +0900

    :+1: change example

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,16 +1,17 @@
 import { init } from "@napolab/worker_thread";
 
-import path from "./test.worker";
+import test1WorkerPath from "./test.worker";
+import test2WorkerPath from "./test2.worker"
 
 export const main = async () => {
-  const worker1 = init(path)({ id: "1" });
-  const worker2 = init(path)({ id: "2" });
+  const worker1 = init(test1WorkerPath)({ id: "worker1", a: 1, b: 2 });
+  const worker2 = init(test2WorkerPath)({ id: "worker2", a: 2, b: 3 });
 
   worker1.on("message", (message) => {
-    console.log(message);
+    console.log("worker1", message);
   });
   worker2.on("message", (message) => {
-    console.log(message);
+    console.log("worker2", message);
   });
 };
 
